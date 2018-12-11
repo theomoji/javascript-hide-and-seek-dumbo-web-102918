a786e9a1882990190ebfd73069b38e77535e1622
github username <github email address>
2018-12-11 12:33:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+  return node;
+}
